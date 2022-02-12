0c4510253b57c100f4c712d2d8223cb03d268ca9
Sahib Singh <75523820+ssahibsingh@users.noreply.github.com>
2022-02-12 12:38:01 +0530

    updated Timer

--- assets/js/main.js
+++ assets/js/main.js
@@ -163,7 +163,7 @@
   /**
    * Timer
    */
-   const end = new Date("Feb 14, 2022 16:19:00").getTime();
+   const end = new Date("Feb 24, 2022 20:59:00").getTime();
    //const end = new Date("November 09, 2020 00:00:00").getTime();
    const dayEl = document.querySelector(".days");
    const hoursEl = document.querySelector(".hours");
